5c9528632559137998fe98d7d1db56058a01dbe6
Shiho <seinoshiho@jouyashihonoMacBook-Air.local>
2022-03-07 12:27:52 +0900

    ロールバックを使いマイグレーションファイル名の編集、ユーザーの編集画面で国、仕事の登録機能追加。 #1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,14 +75,15 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => 'smtp.gmail.com',
-    :user_name => ENV['user_name'], #gmailアドレス
-    :password =>  ENV['password'], #gmailパスワード
+    :user_name => ENV['user_name'],
+    :password =>  ENV['password'],
     :authentication => 'login',
     :enable_starttls_auto => true
   }
--- db/migrate/20220305083605_add_country_and_job_to_users.rb
+++ db/migrate/20220305083605_add_country_and_job_to_users.rb
@@ -1,4 +1,4 @@
-class AddCountryAndJobToUser < ActiveRecord::Migration[6.1]
+class AddCountryAndJobToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :country, :string
     add_column :users, :job, :string
