77f8d51b18f1c85e96a658e3123d8e1341f9dada
shiho-shiro <taleshiho@gmail.com>
2022-08-30 09:35:37 +0900

    carrierwave.rbに追記

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,18 @@
-unless Rails.env.development? || Rails.env.test?
-  require 'carrierwave/storage/abstract'
-  require 'carrierwave/storage/file'
-  require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-
+if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = ENV['AWS_BUCKET']
     config.fog_credentials = {
       provider: 'AWS',
+      region: ENV['AWS_DEFAULT_REGION'],
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_DEFAULT_REGION'],
-      path_style: true
     }
+    config.fog_directory = ENV['AWS_BUCKET']
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
